6d2541510135fe649bac929834155475bce6ab82
Daniel Kim <kkdh1109@gmail.com>
2021-05-16 19:46:30 -0400

    install react-router-dom and setup routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   return (
+    
     <div className="app">
- 
+        <Switch>
+          <Route path='/' component={Home} />
+          {/* <Route path='/about' component={About} /> */}
+        </Switch>
     </div>
   );
 }
