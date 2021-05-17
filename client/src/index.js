b21319eecc952a336ade2a4bea0fd3df3175a476
Daniel Kim <kkdh1109@gmail.com>
2021-05-17 15:21:18 -0400

    create rootReducer to combine ruducers inside reducer component

--- client/src/index.js
+++ client/src/index.js
@@ -10,10 +10,16 @@ import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 
+import Reducer from './components/reducers'
+
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware()}>
+  <Provider store={createStoreWithMiddleware(
+    Reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}>
     <BrowserRouter>
       <React.StrictMode>
         <App />
